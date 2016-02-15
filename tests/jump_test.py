d99545453627c045405493fc3828742daccab199
Max Jonas Werner <mail@makk.es>
2016-02-15 20:32:00 +0100

    Some fixes on matching and sorting
    - When a memoized directory matches the pattern the user entered, it
      is always ranked highest.
    - When following symlinks, J now stores the symlink itself instead of
      the target (os.chdir always follows the symlinks and there's no way
      to not follow it so we have to work around that).
    - Better handling of pattern starting with "/".
    - Added first tests.

---
+++ tests/jump_test.py
@@ -0,0 +1,35 @@
+from collections import Counter
+import jump
+
+
+def sort_history_prefers_exact_matches_test():
+    history = Counter({
+        "/home/max": 3,
+        "/home/max/dev/boplish/core": 4
+    })
+    res = jump.sort_history("/home/max", history, True)
+    assert res[0][0] == "/home/max"
+    res = jump.sort_history("home/max", history, True)
+    assert res[0][0] == "/home/max"
+
+
+def sort_history_prefers_suffix_match_test():
+    history = Counter({
+        "/home/max": 3,
+        "/home/max/dev/boplish/core": 4
+    })
+    res = jump.sort_history("max", history, True)
+    assert res[0][0] == "/home/max"
+    res = jump.sort_history("/max", history, True)
+    assert res[0][0] == "/home/max"
+    res = jump.sort_history("e/max", history, True)
+    assert res[0][0] == "/home/max/dev/boplish/core"
+
+
+def sort_history_test():
+    history = Counter({
+        "/home/max": 3,
+        "/home/max/dev/boplish/core": 4
+    })
+    res = jump.sort_history("/max", history, True)
+    assert res[0][0] == "/home/max"
