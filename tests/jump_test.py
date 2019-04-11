c06839b98feddc66cdd682e436a716b4705ea80d
Max Jonas Werner <mwerner@mesosphere.io>
2019-04-11 12:52:35 +0200

    Added Makefile and converted tests to unittest module

--- tests/jump_test.py
+++ tests/jump_test.py
@@ -1,35 +1,40 @@
 from collections import Counter
 import jump
+import unittest
 
 
-def sort_history_prefers_exact_matches_test():
-    history = Counter({
-        "/home/max": 3,
-        "/home/max/dev/boplish/core": 4
-    })
-    res = jump.sort_history("/home/max", history, True)
-    assert res[0][0] == "/home/max"
-    res = jump.sort_history("home/max", history, True)
-    assert res[0][0] == "/home/max"
+class TestMatchingMethods(unittest.TestCase):
+
+    def test_sort_history_prefers_exact_matches(self):
+        history = Counter({
+            "/home/max": 3,
+            "/home/max/dev/boplish/core": 4
+            })
+        res = jump.sort_history("/home/max", history, True)
+        self.assertEqual(res[0][0], "/home/max")
+        res = jump.sort_history("home/max", history, True)
+        self.assertEqual(res[0][0], "/home/max")
+
+    def test_sort_history_prefers_suffix_match(self):
+        history = Counter({
+            "/home/max": 3,
+            "/home/max/dev/boplish/core": 4
+            })
+        res = jump.sort_history("max", history, True)
+        self.assertEqual(res[0][0], "/home/max")
+        res = jump.sort_history("/max", history, True)
+        self.assertEqual(res[0][0], "/home/max")
+        res = jump.sort_history("e/max", history, True)
+        self.assertEqual(res[0][0], "/home/max")
+
+    def test_sort_history(self):
+        history = Counter({
+            "/home/max": 3,
+            "/home/max/dev/boplish/core": 4
+            })
+        res = jump.sort_history("/max", history, True)
+        self.assertEqual(res[0][0], "/home/max")
 
 
-def sort_history_prefers_suffix_match_test():
-    history = Counter({
-        "/home/max": 3,
-        "/home/max/dev/boplish/core": 4
-    })
-    res = jump.sort_history("max", history, True)
-    assert res[0][0] == "/home/max"
-    res = jump.sort_history("/max", history, True)
-    assert res[0][0] == "/home/max"
-    res = jump.sort_history("e/max", history, True)
-    assert res[0][0] == "/home/max"
-
-
-def sort_history_test():
-    history = Counter({
-        "/home/max": 3,
-        "/home/max/dev/boplish/core": 4
-    })
-    res = jump.sort_history("/max", history, True)
-    assert res[0][0] == "/home/max"
+if __name__ == '__main__':
+    unittest.main()
